d0ededb865ca18234aeebc397dbea06198e0ae60
kev <kevbs.jsx@gmail.com>
2022-11-19 16:33:37 -0500

    rutas agregadas

---
+++ footstats-front/src/pages/equipo/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Equipo = () => {
+  return (
+    <div>Equipo - id</div>
+  )
+}
---
+++ footstats-front/src/pages/partidos/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Partidos = () => {
+  return (
+    <div>index</div>
+  )
+}
