ad0bd1857cceea5fee5c3b85ef472f4ce8644eac
Joaquin Gomez <joagomez2007@gmail.com>
2022-11-21 10:45:37 -0300

    Arreglado animación nav responsive

--- footstats-front/src/components/Nav.jsx
+++ footstats-front/src/components/Nav.jsx
@@ -6,13 +6,22 @@ import { MenuIcon } from '@heroicons/react/outline';
 
 export const Nav = () => {
   const [responsive, setResponsive] = useState(false);
-
-  const handleCheck = () => setResponsive(!responsive);
+  const mobileNavAnimation = () => {
+    let nav = document.getElementById('nav-mb');
+    if (responsive) {
+      nav.style = 'transform: translate(-100%)';
+    } else {
+      nav.style = 'transform: translate(0%)';
+    }
+  };
+  
+  const handleCheck = () => {
+    setResponsive(!responsive);
+    mobileNavAnimation();
+  };
 
   return (
-    <nav
-      className='nav-container'
-    >
+    <nav className='nav-container'>
       <div className='navbar navbar-desktop'>
         <span className='brand'>FOOTSTATS</span>
 
@@ -41,30 +50,26 @@ export const Nav = () => {
         color='#fff'
       ></MenuIcon>
 
-      {responsive ? (
-        <div className='navbar navbar-mobile'>
-          <span className='brand'>FOOTSTATS</span>
+      <div id='nav-mb' className='navbar navbar-mobile'>
+        <span className='brand'>FOOTSTATS</span>
 
-          <div className='buttons'>
-            <form
-              className='flex-center form-search'
-              action='/search'
-              method='post'
-            >
-              <button className='form-button flex-center' type='submit'>
-                <SearchIcon className='form-search__icon' width={20} />
-              </button>
-              <input type='text' name='key' id='key' className='search-bar' />
-            </form>
-            <Droplist title='Ligas'></Droplist>
-            <Droplist title='Equipos'></Droplist>
-            <a href='/partidos'>Partidos</a>
-            <Button>Iniciar Sesión</Button>
-          </div>
+        <div className='buttons'>
+          <form
+            className='flex-center form-search'
+            action='/search'
+            method='post'
+          >
+            <button className='form-button flex-center' type='submit'>
+              <SearchIcon className='form-search__icon' width={20} />
+            </button>
+            <input type='text' name='key' id='key' className='search-bar' />
+          </form>
+          <Droplist title='Ligas'></Droplist>
+          <Droplist title='Equipos'></Droplist>
+          <a href='/partidos'>Partidos</a>
+          <Button>Iniciar Sesión</Button>
         </div>
-      ) : (
-        ''
-      )}
+      </div>
     </nav>
   );
 };
